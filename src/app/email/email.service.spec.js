858ee73c398608b17c58a3b72c475ebbddcd8e6c
apitombeira <andre.pitombeira@ecabo.de>
2015-08-14 15:28:37 +0200

    Create email service tests

--- src/app/email/email.service.spec.js
+++ src/app/email/email.service.spec.js
@@ -2,6 +2,26 @@
   'use strict';
 
   describe('Email Service', function() {
+    var emailService,
+        http;
+
     beforeEach(module('emailClient'));
+
+    beforeEach(inject(function(_emailService_, _$http_) {
+      emailService = _emailService_;
+      http = _$http_;
+    }));
+
+    describe('Get emails', function() {
+      var url = 'app/api/emails.json';
+
+      it('should get emails', function() {
+        spyOn(http, 'get');
+
+        emailService.getEmails();
+
+        expect(http.get).toHaveBeenCalledWith(url);
+      });
+    });
   });
 })();
